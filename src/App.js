2b319977785487905441b294985627d1f0af383d
Condelfbm <luizfelipebmr@gmail.com>
2022-12-27 23:11:02 -0300

    materialize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './Reset.css';
-
+import './app.css';
+import 'materialize-css/dist/css/materialize.min.css'
 
 function App() {
   return (
-    <div className="App">
-      sapato
+    <div className="brown lighten-2">
+      <div className="Title">Folklöre</div>
     </div>
   );
 }
